e56fb34fd7e48e1a51d00d35818926974efaaff2
Azkafar066 <151702847+Azkafar066@users.noreply.github.com>
2024-07-26 11:13:05 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}" ],
+  theme: {
+    extend: {
+    fontFamily:{
+      "Kalnia-Glaze": "Kalnia Glaze",
+      "Sevillana-Static":"Sevillana",
+      "Playwrite-Österreich":"Playwrite AT",
+      
+      
+    }
+    },
+  },
+  plugins: [
+
+  ],
+}
+
